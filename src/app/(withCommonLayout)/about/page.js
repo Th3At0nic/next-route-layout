ea46f9cacf70bed7d2734761edde124a5c9ad76c
Th3At0nic <islammdrahatul@gmail.com>
2025-06-21 17:57:30 +0600

    module.css

--- src/app/(withCommonLayout)/about/page.js
+++ src/app/(withCommonLayout)/about/page.js
@@ -1,9 +1,9 @@
-import React from "react";
+import style from "./Page.module.css";
 
 const AboutPage = () => {
   return (
     <div>
-      <h1 className="text-3xl text-red-500">Hello from about page</h1>
+      <h1 className={style.text_style}>Hello from about page</h1>
     </div>
   );
 };
