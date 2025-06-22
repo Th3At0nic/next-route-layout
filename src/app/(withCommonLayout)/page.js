0c56a55f2372cf752c019a974b62e616e516c062
Th3At0nic <islammdrahatul@gmail.com>
2025-06-23 01:20:42 +0600

    explored revalidate in nextjs, this is used to update the content as per the changes in database data, revalidate is used in fetch method

--- src/app/(withCommonLayout)/page.js
+++ src/app/(withCommonLayout)/page.js
@@ -2,7 +2,9 @@ import ProductCard from "@/components/ProductCard";
 
 const HomePage = async () => {
   const res = await fetch("http://localhost:5000/products", {
-    cache: "force-cache",
+    next: {
+      revalidate: 5, //this is used to revalidate/update the changes of database data automatically
+    },
   });
 
   const products = await res.json();
@@ -13,7 +15,7 @@ const HomePage = async () => {
     <div>
       <h1 className="text-4xl p-5 m-5 text-center">Hello World</h1>
       <h1 className="text-4xl p-5 m-5 text-center">
-        Data Fetching, Caching and revalidatingggg
+        Data Fetching, Caching and revalidating and abc
       </h1>
       <div className="flex gap-5 justify-center">
         {products.map((product) => (
