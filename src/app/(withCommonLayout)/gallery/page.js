5a5832daa96f164c5a93473164944c11e1f6d9b1
Th3At0nic <islammdrahatul@gmail.com>
2025-06-22 18:16:13 +0600

    changed font, optimized font, assets

--- src/app/(withCommonLayout)/gallery/page.js
+++ src/app/(withCommonLayout)/gallery/page.js
@@ -1,18 +1,24 @@
 import Image from "next/image";
 import React from "react";
+import image1 from "../../assets/flower.jpg";
 
 const GalleryPage = () => {
   return (
     <div>
       <h1>This is external image</h1>
       <Image
-        src="https://refine.dev/blog/using-next-image/"
+        src={image1}
+        height={500}
+        width={500}
+        alt="next image optimization"
+      />
+      <Image
+        src="https://img.freepik.com/free-photo/closeup-shot-beautiful-butterfly-with-interesting-textures-orange-petaled-flower_181624-7640.jpg?ga=GA1.1.718440294.1750567385&semt=ais_hybrid&w=740"
         height={500}
         width={500}
         alt="next image optimization"
       />
       <h1>This is normal image</h1>
-      <img src="GalleryPage" width={500} height={500} alt="next image optimization" />
     </div>
   );
 };
